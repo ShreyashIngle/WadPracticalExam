d88ca5aa25bfe3907176da9df57cfe5668c31219
ShreyashIngle <shreyashingle2004@gmail.com>
2025-05-04 12:43:25 +0530

    W9 updated

--- W9/script.js
+++ W9/script.js
@@ -2,23 +2,26 @@ function getWeather() {
   const city = document.getElementById("city").value;
   const result = document.getElementById("result");
 
-  if (!city) return (result.innerHTML = "Please select a city!");
+  if (!city) {
+    result.innerHTML = "Please select a city!";
+    return;
+  }
   result.innerHTML = "Fetching data...";
 
-  fetch("data.json")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = () => {
+    if (xhttp.readyState === 4) {
+      // 4 means the request is complete
+      if (xhttp.status === 200) {
+        const weather = JSON.parse(xhttp.responseText)[city];
+        result.innerHTML = weather
+          ? `<b>Temperature:</b> ${weather.temp}<br><b>Humidity:</b> ${weather.humidity}<br><b>Condition:</b> ${weather.condition}`
+          : "City data not found!";
+      } else {
+        result.innerHTML = "Error fetching data.";
       }
-      return response.json();
-    })
-    .then((data) => {
-      const weather = data[city];
-      result.innerHTML = weather
-        ? `<b>Temperature:</b> ${weather.temp}<br><b>Humidity:</b> ${weather.humidity}<br><b>Condition:</b> ${weather.condition}`
-        : "City data not found!";
-    })
-    .catch(() => {
-      result.innerHTML = "Error fetching data.";
-    });
+    }
+  };
+  xhttp.open("GET", "data.json", true);
+  xhttp.send();
 }
